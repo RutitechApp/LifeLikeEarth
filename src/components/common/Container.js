f06977725bc658fe07932a3ee897c9d397f73a68
Gaurav D <devgauravdobariya@gmail.com>
2024-10-05 17:54:32 +0530

    feat: bg added

--- src/components/common/Container.js
+++ src/components/common/Container.js
@@ -1,26 +1,30 @@
 import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import colors from '../../utils/colors'
+import FastImage from 'react-native-fast-image'
+import imageConstants from '../../utils/imageConstants'
 
-const Container = ({children}) => {
-  return (
-    <View style={styles.container}>
-      <SafeAreaView style = {styles.safeContainer}>
-        {children}
-      </SafeAreaView>
-    </View>
-  )
+const Container = ({ children }) => {
+    return (
+        <View style={styles.container}>
+            <FastImage source={imageConstants.statGif} style={StyleSheet.absoluteFill}>
+                <SafeAreaView style={styles.safeContainer}>
+                    {children}
+                </SafeAreaView>
+            </FastImage>
+        </View>
+    )
 }
 
 export default Container
 
 const styles = StyleSheet.create({
-    container : {
-        flex  : 1,
-        backgroundColor : colors.black,
-        paddingHorizontal : 16
+    container: {
+        flex: 1,
+        backgroundColor: colors.black,
+        paddingHorizontal: 16
     },
-    safeContainer : {
-        flex : 1
+    safeContainer: {
+        flex: 1
     }
 })
--- src/utils/imageConstants.js
+++ src/utils/imageConstants.js
@@ -1,5 +1,6 @@
 export default {
   exoplanetGif: require("../assets/images/Exoplanet.gif"),
   detectionGif: require("../assets/images/Detection.gif"),
-  bannerImage :require('../assets/images/Banner.png')
+  bannerImage :require('../assets/images/Banner.png'),
+  statGif : require('../assets/images/Stars.gif')
 };
