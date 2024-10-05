04a92c15f24ee2f5e7abe4880efd909ded41e528
Tvisha <devtvisha.n@gmail.com>
2024-10-05 11:51:30 +0530

    feat: fonts setup

---
+++ src/utils/fonts.js
@@ -0,0 +1,8 @@
+export default {
+  CrimsonTextBold: "CrimsonText-Bold",
+  CrimsonTextBoldItalic: "CrimsonText-BoldItalic",
+  CrimsonTextItalic: "CrimsonText-Italic",
+  CrimsonTextRegular: "CrimsonText-Regular",
+  CrimsonTextSemiBold: "CrimsonText-SemiBold",
+  CrimsonTextSemiBoldItalic: "CrimsonText-SemiBoldItalic",
+};
