812e529f959047bcf2e44e5c3a3fb516df604efa
Gaurav D <devgauravdobariya@gmail.com>
2024-10-05 12:40:56 +0530

    feat: created dummy data for onboarding

---
+++ src/helper/dummyData.js
@@ -0,0 +1,19 @@
+export const OnBoadingData = [
+    {
+        id: 0,
+        title: "What is Exoplanet?",
+        desc: "What is Exoplanet?",
+        imageUrl: ""
+    },
+    {
+        id: 0,
+        title: "Types of Exoplanet",
+        desc: "Exoplanets can be gas giants or rocky, and may orbit in habitable or extreme zones.",
+        imageUrl: ""
+    }, {
+        id: 0,
+        title: "Detection of Exoplanet ",
+        desc: "Scientists detect exoplanets by observing transits or star wobbles.",
+        imageUrl: ""
+    }
+]
